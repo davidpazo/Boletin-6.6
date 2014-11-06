160e91ba5e5674cfd3a45ead4fa53a23f1f8bde4
dpazolopez <dpazolopez@eniac21i.danielcastelao.org>
2014-11-06 10:35:30 +0100

    finish

---
+++ src/Boletin66/Alumno.java
@@ -0,0 +1,19 @@
+package Boletin66;
+public class Alumno {
+
+    String nombre = "Alumno 1";
+    char grupo = 'A';
+
+    char devolvergrupo() {
+        return grupo;
+    }
+
+    void cambiargrupo(char nuevogrupo) {
+        grupo = nuevogrupo;
+    }
+
+    void imprimir() {
+        System.out.println("Nombre: " + nombre);
+        System.out.println("Grupo: " + devolvergrupo());
+    }
+}    
